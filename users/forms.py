483fc4cc616ce961bcb7ca04bc58da44b41c6dbf
Brenda-M <brendanjuguna1@gmail.com>
2020-05-28 22:39:54 +0300

    add a form class for the loginform

--- users/forms.py
+++ users/forms.py
@@ -5,5 +5,9 @@ class RegistrationForm(forms.Form):
   f_name = forms.CharField(max_length=250, label="Full Name")
   u_name = forms.CharField(max_length=250, label="Username")
   password = forms.CharField(widget=forms.PasswordInput())
+
+class RegistrationForm(forms.Form):
+  email = forms.EmailField(max_length=250, label="Email")
+  password = forms.CharField(widget=forms.PasswordInput())
  
   
