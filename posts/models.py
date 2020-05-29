794c6d4beecda20d9bf76b432e8e34563d834c05
Brenda-M <brendanjuguna1@gmail.com>
2020-05-29 14:45:52 +0300

    create image model

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# class Image(models.Model):
+
+class Image(models.Model):
+  image = models.ImageField(upload_to='img_posts/')
+  img_name = models.CharField(max_length=100)
+  caption = models.CharField(max_length=100)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+  created_at = models.DateField(auto_now_add=True)
+  updated_at = models.DateField()
   
+
+
+
