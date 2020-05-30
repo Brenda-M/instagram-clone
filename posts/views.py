a67855c444154ac8ed0a1a0c25e4b5b772aa7674
Brenda-M <brendanjuguna1@gmail.com>
2020-05-30 15:49:43 +0300

    display images by date created

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Image
 from django.http import HttpResponse
 
 
 def index(request):
-  return render(request, 'posts/index.html')
+  context = {
+    'images': Image.objects.all()
+  }
+  return render(request, 'posts/index.html', context)
+
+class ImageListView(ListView):
+  model = Image  #tells the listview which model to query inorder to create the listview
+  template_name = 'posts/index.html' #naming convention is <app>/<model>_<viewtype>.html
+  context_object_name = 'images'
+  ordering = ['created_at']
