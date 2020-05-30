d8528095ec377d49c41ff7247aeeb8993a2639e0
Brenda-M <brendanjuguna1@gmail.com>
2020-05-30 15:56:32 +0300

    create path for individual post

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
+from .views import ImageListView, ImageDetailView
 from . import views
 
 urlpatterns = [
-  path('', views.index, name='index'),
+  path('', ImageListView.as_view(), name='index'),
+  path('post/<int:pk>/', ImageDetailView.as_view(), name='image-post-detail')
+
 ]
