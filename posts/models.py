96734e26a05a0b9d42fe91cc752718caac4f954c
Brenda-M <brendanjuguna1@gmail.com>
2020-05-30 23:33:40 +0300

    add comments class

--- posts/models.py
+++ posts/models.py
@@ -8,8 +8,9 @@ class Image(models.Model):
   img_name = models.CharField(max_length=100, blank=True)
   image = models.ImageField(upload_to='img_posts/')
   caption = models.CharField(max_length=100, blank=True)
+  likes = models.ManyToManyField(User, related_name='post_likes')
   user = models.ForeignKey(User, on_delete=models.CASCADE)
-  created_at = models.DateField(default=timezone.now)
+  created_at = models.DateField(auto_now_add=True)
   updated_at = models.DateField(auto_now=True)
 
   def __str__(self):
@@ -18,6 +19,16 @@ class Image(models.Model):
   def get_absolute_url(self):
     return reverse('image-post-detail', kwargs={'pk': self.pk})
 
+class Comments(models.Model):
+  comment = models.CharField(max_length=100)
+  author = models.ForeignKey(User, on_delete=models.CASCADE)
+  img_post = models.ForeignKey(Image, on_delete=models.CASCADE)
+  created_at = models.DateField(auto_now_add=True)
+
+  def __str__(self):
+    return self.comment
+
+   
 
   
 
