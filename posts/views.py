12e77960e1d167abea3311324df14b949d9026f0
Brenda-M <brendanjuguna1@gmail.com>
2020-05-30 18:03:20 +0300

    add login required decorator

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Image
 from django.http import HttpResponse
 
@@ -14,8 +15,16 @@ class ImageListView(ListView):
   model = Image  #tells the listview which model to query inorder to create the listview
   template_name = 'posts/index.html' #naming convention is <app>/<model>_<viewtype>.html
   context_object_name = 'images'
-  ordering = ['created_at']
+  ordering = ['-created_at']
 
 class ImageDetailView(DetailView):
   model = Image
-  
+
+class ImageCreateView(LoginRequiredMixin, CreateView):
+  model = Image
+  fields = ['image', 'caption']
+  # success_url=('')
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
