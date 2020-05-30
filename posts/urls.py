6d1e46c7cdd0bb4954194277f10ac241d6abea62
Brenda-M <brendanjuguna1@gmail.com>
2020-05-30 20:37:34 +0300

    add likes url path

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
-from .views import ImageListView, ImageDetailView, ImageCreateView, ImageUpdateView, ImageDeleteView
-from . import views
+from .views import (
+  ImageListView, 
+  ImageDetailView, 
+  ImageCreateView, 
+  ImageUpdateView, 
+  ImageDeleteView, 
+  LikeView
+)
+
 
 urlpatterns = [
   path('', ImageListView.as_view(), name='index'),
@@ -8,5 +15,6 @@ urlpatterns = [
   path('post/new/', ImageCreateView.as_view(), name='image-create'),
   path('post/<int:pk>/update', ImageUpdateView.as_view(), name='image-update'),
   path('post/<int:pk>/delete', ImageDeleteView.as_view(), name='image-delete'),
-
+  path('like/<int:pk>/',  LikeView, name="like_post")
+  # path('find-user/', views.search_results, name='search_results')
 ]
