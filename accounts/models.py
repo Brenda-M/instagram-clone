1f24c71928af100b0fec2c5ca7bdbbd9b60c2147
Brenda-M <brendanjuguna1@gmail.com>
2020-05-31 13:54:03 +0300

    add followers table

--- accounts/models.py
+++ accounts/models.py
@@ -4,7 +4,10 @@ from django.contrib.auth.models import User
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
   image = models.ImageField(default='default.jpg', upload_to='profile_imgs/')
-  bio = models.CharField(max_length=100)
+  bio = models.CharField(max_length=100, blank=True)
+  followers = models.ManyToManyField(User,related_name='is_following', blank=True)
+  created_at = models.DateField(auto_now_add=True)
+  update_at = models.DateField(auto_now=True)
 
   def __str__(self):
     return f'{self.user.username} Profile'
