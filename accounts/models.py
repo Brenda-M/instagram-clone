1e3a51bdd84719f70368258c62c7e3addde36576
Brenda-M <brendanjuguna1@gmail.com>
2020-05-31 19:44:22 +0300

    add profile manager class

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+class ProfileManager(models.Manager):
+  def toggle_follow(self, request_user, username_to_toggle):
+    profile_ = Profile.objects.get(user__username__iexact=username_to_toggle)
+    user = request_user
+    is_following = False
+    if user in profile_.followers.all():
+      profile_.followers.remove(user)
+    else:
+      profile_.followers.add(user)
+      is_following = True
+    return is_following
+
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
   image = models.ImageField(default='default.jpg', upload_to='profile_imgs/')
