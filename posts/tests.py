1a493bad24d77d15771547652d9e04c4ae59c576
Brenda-M <brendanjuguna1@gmail.com>
2020-06-03 21:43:31 +0300

    set up test for image class

--- posts/tests.py
+++ posts/tests.py
@@ -1,12 +1,26 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 from .models import Image
+from .forms import CommentForm
 
 class ImageTestClass(TestCase):
 
   def setUp(self):
-    Image.objects.create(image='photo.jpg', caption="beautiful scenary")
-  
+    User.objects.create(username='testUser', email='test@gmail.com')
+    Image.objects.create(image='photo.jpg', caption="beautiful scenary",user_id='1')
+
   def test_get_absolute_url(self):
+    image = Image.objects.get(id=1)
+    self.assertEquals(image.get_absolute_url(), '/post/1/')
+  
+
+class CommentFormTestClass(TestCase):
+
+  def test_comment_field(self):
+    form = CommentForm()
+    self.assertFalse(form.fields['content'].label==None)
+ 
+
   
 
   
